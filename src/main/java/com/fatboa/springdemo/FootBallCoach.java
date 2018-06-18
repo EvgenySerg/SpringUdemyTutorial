6324901e2ce337e63eca2f17a1b26ef01c67c6cd
fatboa <fatboa@yandex.ru>
2018-06-18 17:34:05 +0300

    first commit

---
+++ src/main/java/com/fatboa/springdemo/FootBallCoach.java
@@ -0,0 +1,7 @@
+package com.fatboa.springdemo;
+
+public class FootBallCoach implements Coach {
+    public String getDailyWorkout() {
+        return "Play football for 2 hours!";
+    }
+}
---
+++ src/main/java/com/fatboa/springdemo/TrackCoach.java
@@ -0,0 +1,9 @@
+package com.fatboa.springdemo;
+
+public class TrackCoach implements Coach {
+    public String getDailyWorkout() {
+        return "Run 5k kilometers/";
+    }
+
+}
+
